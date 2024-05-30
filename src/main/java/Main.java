481980880c02369c8d4e263d94ed01d3a8aa5dea
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-05-30 14:33:23 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.Writer;
 import com.google.zxing.WriterException;
@@ -12,11 +10,10 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws WriterException, IOException {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        
         String url = "https://www.linkedin.com/in/daguilar3/";
         String path = "/Users/danny/Desktop/linkedin.jpg";
-
+        
         BitMatrix matrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, 500, 500);
         MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(path));
         System.out.println("QR CODE read successfully");
